aef1ae487daaf05cf012ac3c4d4ee662109ecd31
Randy <randomzakya@gmail.com>
2022-03-23 17:55:06 +0700

    done shading cube

--- src/model/cube.mjs
+++ src/model/cube.mjs
@@ -1,8 +1,8 @@
 // Now create an array of positions for the cube.
 
 function generateCubeVertice(modelGL) {
-  var q1 = 1;
-  var q2 = 0;
+  var q1 = 0;
+  var q2 = 1;
   var q3 = 2;
   var q4 = 3;
   for (var i = 0; i < CubeVertices / cubeFace; i++) {
@@ -20,318 +20,388 @@ function generateCubeVertice(modelGL) {
 // prettier-ignore
 var positions = [
   // First block
+  // left top bar complete 6 side
+  // top face (urutan bener sesuai kaidah tangan kanan)
   -0.9, 1.1, 0.9,
-  -0.9, 1.1, -0.9, 
-  -1.1, 1.1, 0.9, 
-  -1.1, 1.1, -0.9,
-
-  -0.9, 0.9, 0.9,
-  -0.9, 0.9, -0.9, 
-  -1.1, 0.9, 0.9, 
-  -1.1, 0.9, -0.9,
-
-  -0.9, 0.9, -0.9,
-  -0.9, 1.1, -0.9, 
-  -0.9, 0.9, 0.9, 
-  -0.9, 1.1, 0.9,
-
-  -1.1, 0.9, -0.9,
+  -0.9, 1.1, -0.9,
   -1.1, 1.1, -0.9, 
-  -1.1, 0.9, 0.9, 
-  -1.1, 1.1, 0.9,
+  -1.1, 1.1, 0.9, 
 
+  // bottom face (urutan bener sesuai kaidah tangan kanan)
+  -0.9, 0.9, 0.9,
+  -1.1, 0.9, 0.9, 
+  -1.1, 0.9, -0.9, 
+  -0.9, 0.9, -0.9,
+
+  // right face (urutan bener sesuai kaidah tangan kanan)
+  -0.9, 1.1, -0.9,  
+  -0.9, 1.1, 0.9,
+  -0.9, 0.9, 0.9,
+  -0.9, 0.9, -0.9,
+
+  // left face (urutan bener sesuai kaidah tangan kanan)
+  -1.1, 1.1, -0.9,
   -1.1, 0.9, -0.9,
-  -0.9, 0.9, -0.9, 
+  -1.1, 0.9, 0.9, 
+  -1.1, 1.1, 0.9, 
+
+  // back face (urutan bener sesuai kaidah tangan kanan)
+  -1.1, 0.9, -0.9,
   -1.1, 1.1, -0.9, 
   -0.9, 1.1, -0.9,
+  -0.9, 0.9, -0.9, 
 
+  // front face (urutan bener sesuai kaidah tangan kanan)
   -1.1, 0.9, 0.9,
   -0.9, 0.9, 0.9, 
-  -1.1, 1.1,  0.9, 
   -0.9, 1.1,  0.9,
+  -1.1, 1.1,  0.9, 
 
   // Second block
-  0.9, 1.1, 0.9,
-  0.9, 1.1, -0.9, 
+  // right top bar complete 6 side
+  // top face (urutan bener sesuai kaidah tangan kanan)
   1.1, 1.1, 0.9, 
   1.1, 1.1, -0.9,
+  0.9, 1.1, -0.9, 
+  0.9, 1.1, 0.9,
 
+  // bottom face (urutan bener sesuai kaidah tangan kanan)
+  1.1, 0.9, 0.9, 
   0.9, 0.9, 0.9,
   0.9, 0.9, -0.9, 
-  1.1, 0.9, 0.9, 
   1.1, 0.9, -0.9,
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   0.9, 0.9, -0.9,
-  0.9, 1.1, -0.9, 
   0.9, 0.9, 0.9, 
   0.9, 1.1, 0.9,
+  0.9, 1.1, -0.9, 
 
+  // right face (urutan bener sesuai kaidah tangan kanan)
   1.1, 0.9, -0.9,
-  1.1, 1.1, -0.9, 
-  1.1, 0.9, 0.9, 
+  1.1, 1.1, -0.9,
   1.1, 1.1, 0.9,
+  1.1, 0.9, 0.9,  
 
+  // back face (urutan bener sesuai kaidah tangan kanan)
+  0.9, 1.1, -0.9,
+  1.1, 1.1, -0.9, 
   1.1, 0.9, -0.9,
   0.9, 0.9, -0.9, 
-  1.1, 1.1, -0.9, 
-  0.9, 1.1, -0.9,
 
+  // front face (urutan bener sesuai kaidah tangan kanan)
   1.1, 0.9, 0.9,
-  0.9, 0.9, 0.9, 
-  1.1, 1.1, 0.9, 
+  1.1, 1.1, 0.9,
   0.9, 1.1, 0.9,
+  0.9, 0.9, 0.9,  
 
   // Third block
+  // back top bar incomplete 6 side
+  // bottom face (urutan bener sesuai kaidah tangan kanan)
+  1.1,  0.9, -0.9,
   -1.1, 0.9, -0.9,
-  -1.1, 0.9, -1.1, 
-  1.1,  0.9, -0.9, 
+  -1.1, 0.9, -1.1,  
   1.1,  0.9, -1.1,
 
-  -1.1, 1.1, -0.9,
-  -1.1, 1.1, -1.1, 
-  1.1,  1.1, -0.9, 
+  // top face (urutan bener sesuai kaidah tangan kanan)
+  1.1,  1.1, -0.9,
   1.1,  1.1, -1.1,
+  -1.1, 1.1, -1.1,  
+  -1.1, 1.1, -0.9,
 
+  // back face (urutan bener sesuai kaidah tangan kanan)
   -1.1, 0.9, -1.1,
   -1.1, 1.1, -1.1, 
+  1.1,  1.1, -1.1,
   1.1,  0.9, -1.1, 
-  1.1,  1.1, -1.1,
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   -1.1, 0.9, -0.9,
+  -1.1,  1.1, -0.9,
+  -1.1,  1.1, -1.1, 
   -1.1, 0.9, -1.1, 
-  -1.1,  1.1, -0.9, 
-  -1.1,  1.1, -1.1,
 
-  1.1, 0.9, -0.9,
+  // right face (urutan bener sesuai kaidah tangan kanan)
   1.1, 0.9, -1.1, 
-  1.1,  1.1, -0.9, 
   1.1,  1.1, -1.1,
-
-
+  1.1,  1.1, -0.9, 
+  1.1, 0.9, -0.9,
 
   // forth block
-  -1.1, 0.9, 0.9,
-  -1.1, 0.9, 1.1, 
-  1.1,  0.9, 0.9, 
+  // front top bar incomplete
+  // bottom face (urutan bener sesuai kaidah tangan kanan)
   1.1,  0.9, 1.1,
+  -1.1, 0.9, 1.1, 
+  -1.1, 0.9, 0.9,
+  1.1,  0.9, 0.9, 
 
+  // top face (urutan bener sesuai kaidah tangan kanan)
   -1.1, 1.1, 0.9,
   -1.1, 1.1, 1.1, 
-  1.1,  1.1, 0.9, 
   1.1,  1.1, 1.1,
+  1.1,  1.1, 0.9, 
 
+  // front face (urutan bener sesuai kaidah tangan kanan)
   -1.1, 0.9, 1.1,
-  -1.1, 1.1, 1.1, 
   1.1,  0.9, 1.1, 
   1.1,  1.1, 1.1,
+  -1.1, 1.1, 1.1, 
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   -1.1, 0.9,  0.9,
   -1.1, 0.9,  1.1, 
-  -1.1,  1.1, 0.9, 
   -1.1,  1.1, 1.1,
+  -1.1,  1.1, 0.9, 
 
+  // right face (urutan bener sesuai kaidah tangan kanan)
   1.1, 0.9,  0.9,
-  1.1, 0.9,  1.1, 
   1.1,  1.1, 0.9, 
   1.1,  1.1, 1.1,
+  1.1, 0.9,  1.1, 
   
   // fifth block
+  // bottom left bar complete 6 side
+  // bottom face (urutan bener sesuai kaidah tangan kanan)
   -0.9, -1.1, 0.9,
-  -0.9, -1.1, -0.9, 
   -1.1, -1.1, 0.9, 
   -1.1, -1.1, -0.9,
-
-  -0.9, -0.9, 0.9,
-  -0.9, -0.9, -0.9, 
-  -1.1, -0.9, 0.9, 
-  -1.1, -0.9, -0.9,
-
-  -0.9, -0.9, -0.9,
   -0.9, -1.1, -0.9, 
+
+  // top face (urutan bener sesuai kaidah tangan kanan)
+  -0.9, -0.9, 0.9,
+  -0.9, -0.9, -0.9,
+  -1.1, -0.9, -0.9, 
+  -1.1, -0.9, 0.9, 
+
+  // right face (urutan bener sesuai kaidah tangan kanan)
+  -0.9, -1.1, -0.9, 
+  -0.9, -0.9, -0.9,
   -0.9, -0.9, 0.9, 
   -0.9, -1.1, 0.9,
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -0.9, -0.9,
-  -1.1, -1.1, -0.9, 
+  -1.1, -1.1, -0.9,
+  -1.1, -1.1, 0.9, 
   -1.1, -0.9, 0.9, 
-  -1.1, -1.1, 0.9,
 
+  // back face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -0.9, -0.9,
   -0.9, -0.9, -0.9, 
-  -1.1, -1.1, -0.9, 
   -0.9, -1.1, -0.9,
+  -1.1, -1.1, -0.9, 
 
+  // front face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -0.9, 0.9,
+  -1.1, -1.1,  0.9,
+  -0.9, -1.1,  0.9, 
   -0.9, -0.9, 0.9, 
-  -1.1, -1.1,  0.9, 
-  -0.9, -1.1,  0.9,
 
   // Sixth block
+  // bottom right bar complete 6 side
+  // bottom face (urutan bener sesuai kaidah tangan kanan)
   0.9, -1.1, 0.9,
-  0.9, -1.1, -0.9, 
+  0.9, -1.1, -0.9,
+  1.1, -1.1, -0.9, 
   1.1, -1.1, 0.9, 
-  1.1, -1.1, -0.9,
 
+  // top face (urutan bener sesuai kaidah tangan kanan)
   0.9, -0.9, 0.9,
-  0.9, -0.9, -0.9, 
   1.1, -0.9, 0.9, 
   1.1, -0.9, -0.9,
+  0.9, -0.9, -0.9, 
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   0.9, -0.9, -0.9,
-  0.9, -1.1, -0.9, 
+  0.9, -1.1, -0.9,
+  0.9, -1.1, 0.9, 
   0.9, -0.9, 0.9, 
-  0.9, -1.1, 0.9,
 
+  // right face (urutan bener sesuai kaidah tangan kanan)
   1.1, -0.9, -0.9,
-  1.1, -1.1, -0.9, 
   1.1, -0.9, 0.9, 
   1.1, -1.1, 0.9,
+  1.1, -1.1, -0.9, 
 
+  // back face (urutan bener sesuai kaidah tangan kanan)
   1.1, -0.9, -0.9,
-  0.9, -0.9, -0.9, 
   1.1, -1.1, -0.9, 
   0.9, -1.1, -0.9,
+  0.9, -0.9, -0.9, 
 
+  // front bar (urutan bener sesuai kaidah tangan kanan)
   1.1, -0.9, 0.9,
   0.9, -0.9, 0.9, 
-  1.1, -1.1, 0.9, 
   0.9, -1.1, 0.9,
+  1.1, -1.1, 0.9, 
 
   // Seventh block
+  // bottom back bar incomplete
+  // top face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -0.9, -0.9,
-  -1.1, -0.9, -1.1, 
-  1.1,  -0.9, -0.9, 
+  1.1,  -0.9, -0.9,
   1.1,  -0.9, -1.1,
+  -1.1, -0.9, -1.1,  
 
+  // bottom face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -1.1, -0.9,
   -1.1, -1.1, -1.1, 
+  1.1,  -1.1, -1.1,
   1.1,  -1.1, -0.9, 
-  1.1,  -1.1, -1.1,
 
+  // back face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -0.9, -1.1,
-  -1.1, -1.1, -1.1, 
-  1.1,  -0.9, -1.1, 
+  1.1,  -0.9, -1.1,
   1.1,  -1.1, -1.1,
+  -1.1, -1.1, -1.1,  
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -0.9, -0.9,
   -1.1, -0.9, -1.1, 
-  -1.1, -1.1, -0.9, 
   -1.1, -1.1, -1.1,
+  -1.1, -1.1, -0.9, 
 
+  // right face (urutan bener sesuai kaidah tangan kanan)
   1.1, -0.9, -0.9,
-  1.1, -0.9, -1.1, 
   1.1, -1.1, -0.9, 
   1.1, -1.1, -1.1,
+  1.1, -0.9, -1.1, 
 
 
 
   // forth block
+  // bottom front bar incomplete
+  // top face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -0.9, 0.9,
   -1.1, -0.9, 1.1, 
-  1.1,  -0.9, 0.9, 
   1.1,  -0.9, 1.1,
+  1.1,  -0.9, 0.9, 
 
+  // bottom face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -1.1, 0.9,
-  -1.1, -1.1, 1.1, 
   1.1,  -1.1, 0.9, 
   1.1,  -1.1, 1.1,
+  -1.1, -1.1, 1.1, 
 
+  // front face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -0.9, 1.1,
   -1.1, -1.1, 1.1, 
-  1.1,  -0.9, 1.1, 
   1.1,  -1.1, 1.1,
+  1.1,  -0.9, 1.1, 
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   -1.1, -0.9,  0.9,
-  -1.1, -0.9,  1.1, 
   -1.1, -1.1, 0.9, 
   -1.1, -1.1, 1.1,
+  -1.1, -0.9,  1.1, 
 
+  // right face (urutan bener sesuai kaidah tangan kanan)
   1.1, -0.9,  0.9,
-  1.1, -0.9,  1.1, 
+  1.1, -0.9,  1.1,
+  1.1, -1.1, 1.1, 
   1.1, -1.1, 0.9, 
-  1.1, -1.1, 1.1,
 
   // ninth block
+  // right back bar incomplete
+  // back face (urutan bener sesuai kaidah tangan kanan)
   0.9,   0.9, -1.1,
   1.1,   0.9, -1.1, 
-  0.9,  -0.9, -1.1, 
   1.1,  -0.9, -1.1,
+  0.9,  -0.9, -1.1, 
 
+  // front face (urutan bener sesuai kaidah tangan kanan)
   0.9,   0.9, -0.9,
-  1.1,   0.9, -0.9, 
   0.9,  -0.9, -0.9, 
   1.1,  -0.9, -0.9,
+  1.1,   0.9, -0.9, 
 
+  // right face (urutan bener sesuai kaidah tangan kanan)
   1.1,   0.9, -0.9,
-  1.1,   0.9, -1.1, 
   1.1,  -0.9, -0.9, 
   1.1,  -0.9, -1.1,
+  1.1,   0.9, -1.1, 
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   0.9,   0.9, -0.9,
   0.9,   0.9, -1.1, 
-  0.9,  -0.9, -0.9, 
   0.9,  -0.9, -1.1,
+  0.9,  -0.9, -0.9, 
 
   // tenth block
+  // right front bar incomplete
+  // front face (urutan bener sesuai kaidah tangan kanan)
   0.9,   0.9, 1.1,
-  1.1,   0.9, 1.1, 
   0.9,  -0.9, 1.1, 
   1.1,  -0.9, 1.1,
+  1.1,   0.9, 1.1, 
 
+  // back face (urutan bener sesuai kaidah tangan kanan)
   0.9,   0.9, 0.9,
   1.1,   0.9, 0.9, 
-  0.9,  -0.9, 0.9, 
   1.1,  -0.9, 0.9,
+  0.9,  -0.9, 0.9, 
 
+  // right face (urutan bener sesuai kaidah tangan kanan)
   1.1,   0.9, 0.9,
   1.1,   0.9, 1.1, 
-  1.1,  -0.9, 0.9, 
   1.1,  -0.9, 1.1,
+  1.1,  -0.9, 0.9, 
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   0.9,   0.9, 0.9,
-  0.9,   0.9, 1.1, 
-  0.9,  -0.9, 0.9, 
+  0.9,  -0.9, 0.9,
   0.9,  -0.9, 1.1,
+  0.9,   0.9, 1.1,  
 
   // eleventh block
+  // left front bar incomplete
+  // front face (urutan bener sesuai kaidah tangan kanan)
   -0.9,   0.9, 1.1,
   -1.1,   0.9, 1.1, 
-  -0.9,  -0.9, 1.1, 
   -1.1,  -0.9, 1.1,
+  -0.9,  -0.9, 1.1, 
 
+  // back face (urutan bener sesuai kaidah tangan kanan)
   -0.9,   0.9, 0.9,
-  -1.1,   0.9, 0.9, 
   -0.9,  -0.9, 0.9, 
   -1.1,  -0.9, 0.9,
+  -1.1,   0.9, 0.9, 
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   -1.1,   0.9, 0.9,
-  -1.1,   0.9, 1.1, 
   -1.1,  -0.9, 0.9, 
   -1.1,  -0.9, 1.1,
+  -1.1,   0.9, 1.1, 
 
+  // right face (urutan bener sesuai kaidah tangan kanan)
   -0.9,   0.9, 0.9,
   -0.9,   0.9, 1.1, 
-  -0.9,  -0.9, 0.9, 
   -0.9,  -0.9, 1.1,
+  -0.9,  -0.9, 0.9, 
 
   // twelveth block
+  // left back bar incomplete
+  // back face (urutan bener sesuai kaidah tangan kanan)
   -0.9,   0.9, -1.1,
-  -1.1,   0.9, -1.1, 
   -0.9,  -0.9, -1.1, 
   -1.1,  -0.9, -1.1,
+  -1.1,   0.9, -1.1, 
 
+  // front face (urutan bener sesuai kaidah tangan kanan)
   -0.9,   0.9, -0.9,
   -1.1,   0.9, -0.9, 
-  -0.9,  -0.9, -0.9, 
   -1.1,  -0.9, -0.9,
+  -0.9,  -0.9, -0.9, 
 
+  // left face (urutan bener sesuai kaidah tangan kanan)
   -1.1,   0.9, -0.9,
   -1.1,   0.9, -1.1, 
-  -1.1,  -0.9, -0.9, 
   -1.1,  -0.9, -1.1,
+  -1.1,  -0.9, -0.9, 
 
+  // right face (urutan bener sesuai kaidah tangan kanan)
   -0.9,   0.9, -0.9,
-  -0.9,   0.9, -1.1, 
   -0.9,  -0.9, -0.9, 
   -0.9,  -0.9, -1.1,
+  -0.9,   0.9, -1.1, 
 ];
